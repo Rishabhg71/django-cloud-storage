da8b819a13ac4fba4665f300b797c909b895785d
RG7279805 <47270636+RG7279805@users.noreply.github.com>
2021-04-12 22:54:13 +0530

    Update views.py

--- main/filesys/views.py
+++ main/filesys/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import uploadform
 from .models import upload_model
-# ,upload_doc
 from django.shortcuts import redirect
 from django.views.static import serve
 import os
@@ -11,7 +10,7 @@ from django.urls import path
 from home import views as views
 
 def allfiles(request):
-    # return render(request,'index.html',params)
+    
     return render(request,'filesys/files.html')
 
 
@@ -25,7 +24,7 @@ def fileupload(request):
 
 
 def handle_uploaded_file(f,user_folder,path):  
-    # with open( 'media/'+ user_folder + '/'+f.name, 'wb+') as destination:  
+ 
     with open( str(path) + '/'+f.name, 'wb+') as destination:
         for chunk in f.chunks():  
             destination.write(chunk)
@@ -34,23 +33,19 @@ def handle_uploaded_file(f,user_folder,path):
 def fileuploading(request):  
     if request.method == 'POST':  
         form = uploadform(request.POST, request.FILES)
-        # upload_model.user_name = request.session.get('username')
-        # print('session uname',upload_model.user_name)
-        # file1 = upload_model(user_folder=request.session.get('username'),file_name=request.FILES['file_to_upload'].name,file_uploaded=request.FILES['file_to_upload'])
-        # file1.save()
         try:
             if form.is_valid():
                 print('form vaild') 
                 handle_uploaded_file(request.FILES['file_to_upload'], request.session.get('username'),request.POST.get('folder'))
-                # handle_uploaded_file(request.FILES['file'])  
+        
                 return redirect('../index/'+ request.POST.get('folder'))
-                # return HttpResponse("File uploaded successfuly")  
+  
             else:  
                 form = uploadform()  
                 return render(request,"index.html",{'form':form})
         except Exception as e:
             return HttpResponse(str(e))
-            # return HttpResponse("maybe file is uploaded but exeption occured")
+
 
 
 
