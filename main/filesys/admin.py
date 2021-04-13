effa29d2032c2b3db7e8e540643b52d12a332d12
RG7279805 <47270636+RG7279805@users.noreply.github.com>
2021-04-13 09:00:33 +0530

    Update admin.py

--- main/filesys/admin.py
+++ main/filesys/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 # Register your models here.
 
 from . models import upload_model
-# ,upload_doc
 admin.site.register(upload_model)
-# admin.site.register(upload_doc)
